5ef7b5201fa0c5eb2bad57bbad06bb6f0b3fa0b1
shmilycherish <shmilycherish@gmail.com>
2013-03-19 20:40:35 +0800

    add

---
+++ src/main/java/RichMap/RichGameMap.java
@@ -0,0 +1,104 @@
+package RichMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cherish
+ * Date: 13-3-18
+ * Time: 下午10:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class RichGameMap {
+    private List<Ground> groundList=new ArrayList<Ground>();;
+
+    public  RichGameMap(){
+        initalizeMap();
+    }
+    private void initalizeMap() {
+        groundList.clear();
+
+        initStartGround();
+        initAreaOne();
+        initHospitalGround();
+        initAreaTwo();
+        initToyGround();
+        intiAreaThree();
+        initGiftGround();
+        intiAreaFour();
+        initPrisonGround();
+        intiAreaFive();
+        initMagicGround();
+        initPointsGround();
+    }
+
+    private void initStartGround() {
+        groundList.add(new StartGround());
+
+    }
+
+    private void initAreaOne() {
+        for (int i = 0; i < 13; i++) {
+            groundList.add(new HouseGround(200));
+        }
+    }
+
+    private void initHospitalGround() {
+        groundList.add(new HospitalGround());
+    }
+
+    private void initAreaTwo() {
+        for (int i = 0; i < 13; i++) {
+            groundList.add(new HouseGround(200));
+        }
+    }
+
+    private void initToyGround() {
+        groundList.add(new ToyGround());
+    }
+
+    private void intiAreaThree() {
+        for (int i = 0; i < 6; i++) {
+            groundList.add(new HouseGround(500));
+        }
+    }
+
+    private void initGiftGround() {
+        groundList.add(new GiftGround());
+    }
+
+    private void intiAreaFour() {
+        for (int i = 0; i < 13; i++) {
+            groundList.add(new HouseGround(300));
+        }
+    }
+
+    private void initPrisonGround() {
+        groundList.add(new PrisonGround());
+    }
+
+    private void intiAreaFive() {
+        for (int i = 0; i < 13; i++) {
+            groundList.add(new HouseGround(300));
+        }
+    }
+
+    private void initMagicGround() {
+        groundList.add(new MagicGround());
+    }
+
+    private void initPointsGround() {
+        int[] points={20, 80, 100, 40, 80, 60};
+        for (int i = 0; i < 6; i++) {
+            groundList.add(new PointsGround(points[i]));
+        }
+    }
+
+
+
+
+    public List<Ground> getMap() {
+        return this.groundList;
+    }
+}
--- src/test/java/RichMap/RichMapTest.java
+++ src/test/java/RichMap/RichMapTest.java
@@ -1,5 +1,15 @@
 package RichMap;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created with IntelliJ IDEA.
  * User: cherish
@@ -8,5 +18,74 @@ package RichMap;
  * To change this template use File | Settings | File Templates.
  */
 public class RichMapTest {
+    @Test
+    public void shouldGetCorrectInformationWhenInitializeRichMap() {
+        RichGameMap richMap=new RichGameMap() ;
+        List<Ground> grounds = richMap.getMap();
+
+        assertThat(grounds.size(), is(70));
+        assertThat(grounds.get(0).getDisplayName(),is("S"));
+        assertThat(grounds.get(1),isHouseGroundAs("0",200));
+        assertThat(grounds.get(13),isHouseGroundAs("0",200));
+        assertThat(grounds.get(14).getDisplayName(),is("H"));
+        assertThat(grounds.get(15),isHouseGroundAs("0",200));
+        assertThat(grounds.get(27),isHouseGroundAs("0",200));
+        assertThat(grounds.get(28).getDisplayName(),is("T"));
+        assertThat(grounds.get(29),isHouseGroundAs("0",500));
+        assertThat(grounds.get(34),isHouseGroundAs("0",500));
+        assertThat(grounds.get(35).getDisplayName(),is("G"));
+        assertThat(grounds.get(36),isHouseGroundAs("0",300));
+        assertThat(grounds.get(48),isHouseGroundAs("0",300));
+        assertThat(grounds.get(49).getDisplayName(),is("P"));
+        assertThat(grounds.get(50),isHouseGroundAs("0",300));
+        assertThat(grounds.get(62),isHouseGroundAs("0",300));
+        assertThat(grounds.get(63).getDisplayName(),is("M"));
+        assertThat(grounds.get(64),isPointsGroundAs("$",20));
+        assertThat(grounds.get(69),isPointsGroundAs("$",60));
+    }
+
+    private Matcher<? super Ground> isHouseGroundAs(final String displayName, final int price) {
+        return new TypeSafeMatcher<Ground>() {
+            @Override
+            protected boolean matchesSafely(Ground ground) {
+                HouseGround houseGround=(HouseGround)ground;
+                return houseGround.getDisplayName().equals(displayName) && (houseGround.getPrice() == price);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+            }
+
+            @Override
+            protected void describeMismatchSafely(Ground ground, Description mismatchDescription) {
+                HouseGround houseGround=(HouseGround)ground;
+                String errorMessage = "The ground shouldGetInitStatus be with displayName: " + displayName + ", price: " ;
+                errorMessage += "but in fact is (" + houseGround.getDisplayName() + " " +houseGround.getPrice() +" )";
+                mismatchDescription.appendText(errorMessage);
+            }
+        };
+    }
+
+    private Matcher<? super Ground> isPointsGroundAs(final String displayName, final int points) {
+        return new TypeSafeMatcher<Ground>() {
+            @Override
+            protected boolean matchesSafely(Ground ground) {
+               PointsGround pointsGround=(PointsGround)ground;
+                return pointsGround.getDisplayName().equals(displayName) && (pointsGround.getPoints() == points);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+            }
+
+            @Override
+            protected void describeMismatchSafely(Ground ground, Description mismatchDescription) {
+                PointsGround pointsGround=(PointsGround)ground;
+                String errorMessage = "The ground shouldGetInitStatus be with displayName: " + displayName + ", price: " ;
+                errorMessage += "but in fact is (" +pointsGround.getDisplayName() + " " +pointsGround.getPoints() +" )";
+                mismatchDescription.appendText(errorMessage);
+            }
+        };
+    }
 
 }
